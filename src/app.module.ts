2daf6af4d4656125dc83fdc013e5bdaefbed6b13
aje14700 <36162221+aedelbro@users.noreply.github.com>
2022-05-13 19:23:33 -0400

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntryController } from './entry/entry.controller';
+import { ImageController } from './image/image.controller';
+import { CompanyController } from './company/company.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, EntryController, ImageController, CompanyController],
   providers: [AppService],
 })
 export class AppModule {}
